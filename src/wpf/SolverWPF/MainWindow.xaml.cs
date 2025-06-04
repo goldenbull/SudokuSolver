d7df6b6873c3a547da09a85b40e344add3271431
cjn <cjn@home>
2025-06-04 23:04:16 +0800

    code clean up

--- src/wpf/SolverWPF/MainWindow.xaml.cs
+++ src/wpf/SolverWPF/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using DevExpress.Xpf.Core;
-using libSolver;
 
 namespace SolverWPF
 {
--- src/wpf/libSolver/Region.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace libSolver
-{
-    public class Region
-    {
-        public Cell[,] Cells { get; } = new Cell[3, 3];
-    }
-}
